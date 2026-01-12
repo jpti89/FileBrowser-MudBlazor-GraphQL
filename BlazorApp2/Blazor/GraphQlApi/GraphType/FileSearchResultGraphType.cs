798d2b6f455b95150cc67b309c1e2c34f10b5c5a
Juan Torres <jtorres@cadencesolutions.ca>
2026-01-12 10:36:29 -0700

    Added GraphQL GraphTypes and dependencies

---
+++ BlazorApp2/Blazor/GraphQlApi/GraphType/FileSearchResultGraphType.cs
@@ -0,0 +1,21 @@
+﻿using GraphQL.Types;
+using BlazorApp2.SharedCode.Models.Partials;
+
+namespace BlazorApp2.Blazor.GraphQlApi.GraphType
+{
+    public class FileSearchResultGraphType : ObjectGraphType<FileSearchResult>
+    {
+        public FileSearchResultGraphType()
+        {
+            Name = "FileSearchResult";
+            Description = "Result of a directory crawl operation";
+
+            Field<ListGraphType<FileSystemItemGraphType>, List<FileSystemItem>>("items")
+                .Resolve(context => context.Source.Items)
+                .Description("List of files and directories found");
+
+            Field(x => x.TotalCount, nullable: false).Description("Total number of items found");
+            Field(x => x.SearchPath, nullable: false).Description("The path that was searched");
+        }
+    }
+}
---
+++ BlazorApp2/Blazor/GraphQlApi/GraphType/FileSystemItemGraphType.cs
@@ -0,0 +1,21 @@
+﻿using GraphQL.Types;
+using BlazorApp2.SharedCode.Models.Partials;
+
+namespace BlazorApp2.Blazor.GraphQlApi.GraphType
+{
+    public class FileSystemItemGraphType : ObjectGraphType<FileSystemItem>
+    {
+        public FileSystemItemGraphType()
+        {
+            Name = "FileSystemItem";
+            Description = "Represents a file or directory in the file system";
+
+            Field(x => x.Name, nullable: false).Description("Name of the file or directory");
+            Field(x => x.FullPath, nullable: false).Description("Full path of the file or directory");
+            Field(x => x.IsDirectory, nullable: false).Description("Indicates if this is a directory");
+            Field(x => x.Size, nullable: false).Description("Size in bytes (0 for directories)");
+            Field(x => x.LastModified, nullable: false).Description("Last modification date");
+            Field(x => x.Extension, nullable: false).Description("File extension (empty for directories)");
+        }
+    }
+}
